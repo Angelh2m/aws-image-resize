05a17386a53ca437fef882df8e5aa26d982e6ecf
Angel Hernandez <ahm@Angels-MBP.lan1>
2018-11-03 23:06:57 -0500

    Add Sharp Images

---
+++ index.js
@@ -0,0 +1,107 @@
+const serverless = require('serverless-http');
+const express = require('express')
+const app = express()
+const https = require('https');
+const sharp = require('sharp');
+var imageUri = 'https://avatars1.githubusercontent.com/u/416456?s=460&v=4';
+
+const fs = require('fs');
+
+
+app.get('/', function (req, res) {
+
+  return https.get(imageUri, (resp) => {
+    let body = '';
+
+    resp.setEncoding('base64');
+
+    resp.on('data', (data) => {
+      body += data
+    });
+
+    resp.on('end', () => {
+      var img = new Buffer(body, 'base64');
+
+      sharp(img)
+        .rotate()
+        .resize(200)
+        .toBuffer()
+        .then(data => {
+
+          res.set('Content-Type', 'image/jpg');
+          res.send(data)
+          // res.pipe(res);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+
+
+
+    });
+  }).on('error', (e) => {
+    console.log(`Got error: ${e.message}`);
+  });
+
+})
+
+module.exports.handler = serverless(app);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log('Server Running');
+});
+
+// 'use strict';
+
+// const http = require('http');
+// const https = require('https');
+// const sharp = require('sharp');
+
+// var imageUri = 'https://c2.staticflickr.com/2/1723/27650518707_a230a7ccfa_c.jpg';
+
+// module.exports.hello = (event, context, callback) => {
+//   var body = '';
+//   https.get(imageUri, (resp) => {
+
+//     resp.setEncoding('base64');
+
+//     resp.on('data', (data) => {
+//       body += data
+//     });
+
+//     resp.on('end', () => {
+
+//       let imgBuffer = Buffer.from(body, 'base64');
+//       sharp(imgBuffer)
+//         .rotate()
+//         .resize(200)
+//         .toBuffer()
+//         .then(data => {
+
+//           const res = {
+//             statusCode: 200,
+//             isBase64Encoded: true,
+//             headers: {
+//               'Content-Type': 'image/jpg',
+//             },
+//             body: data
+//           };
+
+//           callback(null, res);
+
+//         })
+//         .catch(err => {
+//           console.log(err);
+//         });
+
+
+
+//     });
+//   }).on('error', (e) => {
+//     console.log(`Got error: ${e.message}`);
+//   });
+
+
+// };
