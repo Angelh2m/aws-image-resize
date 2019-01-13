bbf6f5333a340be9cd10bcf0cc20330bb44e453a
Angel Hernandez <ahm@Angels-MBP.lan1>
2019-01-13 11:19:22 -0600

    Adjustments

--- index.js
+++ index.js
@@ -3,42 +3,35 @@ const express = require('express')
 const app = express()
 const https = require('https');
 const sharp = require('sharp');
-var imageUri = 'https://s3.amazonaws.com/livingwithannah/LivingAnnah-BeefBarbacoa-InstantPot-medium.jpg';
+// var imageUri = 'https://s3.amazonaws.com/livingwithannah/LivingAnnah-BeefBarbacoa-InstantPot-medium.jpg';
+var imageUri = 'https://avatars1.githubusercontent.com/u/416456?s=460&v=4';
 
 
-app.get('/:image/:size?', function (req, res) {
+app.get('/:size/:image?', function (req, res) {
   // console.log(req.params.image);
-  const toSize = req.params.size;
+  const toSize = Number(req.params.size);
 
   return https.get(imageUri, (resp) => {
     let body = '';
 
     resp.setEncoding('base64');
-
     resp.on('data', (data) => {
       body += data
     });
-    res.contentType = 'image/jpeg';
-    resp.on('end', () => {
 
+    resp.on('end', () => {
       var img = new Buffer(body, 'base64');
       return sharp(img)
-        .rotate()
-        .resize(Number(toSize))
+        .resize(toSize)
+        .jpeg({ quality: 90 })
         .toBuffer()
         .then(data => {
-
           res.set({
             'Content-Type': 'image/jpeg',
             'Content-Length': data.length,
             'isBase64Encoded': true
           });
-          var image = new Buffer(data, 'base64');
-          return image
-        }).then(converted => {
-          // console.log('IMAGE ', converted);
-          res.send(converted)
-          // res.end(converted, 'base64');
+          res.send(data)
         })
         .catch(err => {
           console.log(err);
